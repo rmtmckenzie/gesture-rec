1bb57bd898865db7288434b5c73d2dd7ca5c4338
Morgan McKenzie <rmtmckenzie@gmail.com>
2015-11-08 16:11:56 +0000

    Remove website from master branch and move qmlapp up to base directory

--- website/resources/js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(resizeYoutubeVideo);
-
-$(window).resize(resizeYoutubeVideo);
-
-function resizeYoutubeVideo() {
-    var demoVideo = $("#indexVideo > iframe");
-    demoVideo.height((demoVideo.width() / 1.33) - 10);
-    console.log("hi");
-}
